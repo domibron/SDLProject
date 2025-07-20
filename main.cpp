9cfcbe4df96a252d98f0ed1004c6440f2af52d31
DM <dommcneill@outlook.com>
2025-07-20 22:43:47 +0100

    Implemented SDL
    Week 1 completed ish

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include "SDL.h"
 
-int main() {
-	std::cout << "Hello World" << std::endl;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		cout << "Failed to initialise SDL. SDL error: " << SDL_GetError() << endl;
+	}
+
+	SDL_Window* sdlWindow = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, NULL);
+
+	if (sdlWindow == nullptr) {
+		cout << "Failed to create window. SDL error: " << SDL_GetError() << endl;
+	}
+
+	getchar();
+
+	SDL_DestroyWindow(sdlWindow);
+	SDL_Quit();
 
 	return 0;
 }
