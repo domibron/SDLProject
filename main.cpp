7622ffc3705a36e1235f1910c59a5f5e737b25a7
DM <dommcneill@outlook.com>
2025-08-13 19:44:49 +0100

    Got a image to load

--- main.cpp
+++ main.cpp
@@ -15,8 +15,26 @@ int main(int argc, char* argv[]) {
 		cout << "Failed to create window. SDL error: " << SDL_GetError() << endl;
 	}
 
+	SDL_Surface* image = SDL_LoadBMP("door.bmp");
+	if (image == nullptr) {
+		cout << "Failed to load door.bmp. SDL error: " << SDL_GetError() << endl;
+	}
+
+	SDL_Surface* windowSurface = SDL_GetWindowSurface(sdlWindow);
+	if (windowSurface == nullptr) {
+		cout << "Failed to get window surface. SDL error: " << SDL_GetError() << endl;
+	}
+
+	SDL_Rect destinationRect{ 25, 25, 16, 16 };
+	SDL_BlitSurface(image, NULL, windowSurface, &destinationRect);
+
+
+	SDL_UpdateWindowSurface(sdlWindow);
+
+	// pauses console.
 	getchar();
 
+	SDL_FreeSurface(image);
 	SDL_DestroyWindow(sdlWindow);
 	SDL_Quit();
 
